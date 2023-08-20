204fd033644ec8b75ed816e5c53ce41b86e60a77
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2023-08-20 15:14:27 -0300

    feat: create global style file

---
+++ src/styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+@import url(https://db.onlinewebfonts.com/c/19b8f54eb1f074fb01efb1c4f02e9623?family=AvertaStd-Regular);
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'AvertaStd-Regular', sans-serif;
+  color: #003349;
+  background-color: transparent;
+}`;
+
